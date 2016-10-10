c956d5ca275691b44e2dab3df96e33f5523946b5
Ruslan <drumm2k@gmail.com>
2016-10-10 23:41:41 +0300

    Настройка автоматизации

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,14 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var svgstore = require("gulp-svgstore");
+var svgmin = require("gulp-svgmin");
 var server = require("browser-sync").create();
+var run = require("run-sequence");
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
@@ -17,11 +24,16 @@ gulp.task("style", function() {
         "last 2 Chrome versions",
         "last 2 Firefox versions",
         "last 2 Opera versions",
-        "last 2 Edge versions"
-      ]})
+        "last 2 Edge versions"]}),
+      mqpacker({
+        sort: true
+      })
     ]))
     .pipe(gulp.dest("css"))
-    .pipe(server.stream());
+    .pipe(minify())
+    .pipe(server.stream())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("css"));
 });
 
 gulp.task("serve", ["style"], function() {
@@ -34,4 +46,27 @@ gulp.task("serve", ["style"], function() {
 
   gulp.watch("sass/**/*.{scss,sass}", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
+});
+
+gulp.task("images", function() {
+  return gulp.src("img/**/*.{png,jpg,gif}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizaionLevel: 3}),
+      imagemin.jpegtran({progressive: ture})
+    ]))
+    .pipe(gulp.dest("img"));
+});
+
+gulp.task("symbols", function() {
+    return gulp.src("img/icons/*.svg")
+      .pipe(svgmin())
+      .pipe(svgstore({
+        inlineSvg: true
+      }))
+      .pipe(rename("symbols.svg"))
+      .pipe(gulp.dest("img"));
+});
+
+gulp.task("build", function(fn) {
+  run("style", "images", "symbols", fn);
 });
